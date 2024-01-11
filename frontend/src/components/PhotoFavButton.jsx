fc94d7206ab02409818b39b7e23aee26442480a3
joycuicui <cuiqijoy@outlook.com>
2024-01-11 14:58:20 -0800

    make FavBadge red when there are favorited photos

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,20 +4,6 @@ import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton({ id, favorites, onClickFav }) {
-  // const [selected, setSelected] = useState(false);
-  // const [favorites, setFavorites] = useState([]);
-
-  // const handleClickFav = () => {
-  //   setSelected((s) => !s);
-  //   setFavorites((prevF) => {
-  //     if (selected) {
-  //       return prevF.filter((photoId) => photoId !== id);
-  //     } else {
-  //       return [...prevF, id];
-  //     }
-  //   });
-  // };
-
   return (
     <div className="photo-list__fav-icon" onClick={() => onClickFav(id)}>
       <div className="photo-list__fav-icon-svg">
