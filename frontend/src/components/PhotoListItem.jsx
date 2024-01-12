bf8fdc6faa32c7c4895ba63eceb5e7fc219f344b
joycuicui <cuiqijoy@outlook.com>
2024-01-12 11:47:58 -0800

    add an empty Modal when click on the photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
 
 const PhotoListItem = ({ location, urls, user, id, favorites, onClickFav }) => {
+  const [modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  };
+
   return (
     <div className="photo-list__item">
       <PhotoFavButton id={id} favorites={favorites} onClickFav={onClickFav} />
-      <img className="photo-list__image" src={urls.regular} alt="image" />
+      <img
+        onClick={openModal}
+        className="photo-list__image"
+        src={urls.regular}
+        alt="image"
+      />
+      <PhotoDetailsModal isOpen={modal} />
       <div className="photo-list__user-details">
         <img
           className="photo-list__user-profile"
